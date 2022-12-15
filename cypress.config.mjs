2dfd8a22901e5f76d0c28bbe77f1c4614ceed5d6
Eduardo San Martin Morote <posva13@gmail.com>
2022-12-15 09:54:42 +0100

    chore: up cypress

---
+++ cypress.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+
+  e2e: {
+    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
