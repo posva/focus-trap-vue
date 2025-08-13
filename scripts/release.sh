f101bb7472e45577ed1c457995ecc3da5bffb4f1
Eduardo San Martin Morote <posva13@gmail.com>
2025-08-13 09:33:04 +0200

    build: move to pnpm

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,25 +3,23 @@ echo "Current version:" $(grep version package.json | sed -E 's/^.*"([0-9][^"]+)
 echo "Enter version e.g., 1.0.2: "
 read VERSION
 
-
 read -p "Releasing v$VERSION - are you sure? (y/n)" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Releasing v$VERSION ..."
 
   # clear existing ts cache
   rm -rf dist node_modules/.rts2_cache
-  yarn run build
-  yarn run build:dts
+  pnpm run build
+  pnpm run build:dts
 
   # generate the version so that the changelog can be generated too and included
   # in the release
-  yarn version --no-git-tag-version --no-commit-hooks --new-version $VERSION
+  pnpm version --no-git-tag-version --no-commit-hooks --new-version $VERSION
 
   # changelog
-  yarn run changelog
-  yarn prettier --write CHANGELOG.md
+  pnpm run changelog
+  pnpm exec prettier --write CHANGELOG.md
   echo "Please check the git history and the changelog and press enter"
   read OKAY
 
@@ -32,7 +30,7 @@ then
 
   # commit
   # use --tag to publish to a different tag
-  yarn publish --tag latest --new-version "$VERSION" --no-commit-hooks --no-git-tag-version
+  pnpm publish --tag latest --new-version "$VERSION" --no-commit-hooks --no-git-tag-version
 
   # publish
   git push origin refs/tags/v$VERSION
